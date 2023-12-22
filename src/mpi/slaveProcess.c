3e6d5a6bf8553f53c76e0a214899905a015c3c35
Dimimpanti <dimimpanti@gmail.com>
2023-12-22 19:07:00 +0200

    Slave function

--- src/mpi/slaveProcess.c
+++ src/mpi/slaveProcess.c
@@ -141,12 +141,13 @@ void slaveProcess(int master_rank, int min_rank, int max_rank, Info *info, MPI_C
             MPI_COMM_WORLD
     ); 
     
-    PivotedArrays result;
+    PivotedArrays *result;
 
-    //split every process to left and right subarrays accordingly to pivot 
-    result = partition(info->A, info->size, info->pivot);
+    //split every process to left, middle and right sub-arrays accordingly to pivot 
+    result = partition(info->A, info->size, info->pivot);  // TODO: free this
 
-    int partitionResult[3] = {result.leftSize, result.middleSize, result.rightSize};
+    int partitionResult[3] = {result->leftSize, result->middleSize, result->rightSize};
+    
     // Send the sizes of the partitioned array to master process
     MPI_Send(
         partitionResult,
@@ -157,10 +158,11 @@ void slaveProcess(int master_rank, int min_rank, int max_rank, Info *info, MPI_C
         communicator
     );
 
-    // A variable that if it is one we keep the left aarray ,if it is 2 we keep the middle
+    // A variable that if it is one we keep the left array, if it is 2 we keep the middle
     // and if it is 3 we keep the right
     int status;
 
+    // Receive the status from the master process
     MPI_Bcast(
         &status,
         1,
@@ -169,62 +171,110 @@ void slaveProcess(int master_rank, int min_rank, int max_rank, Info *info, MPI_C
         communicator
     );
 
+    // If the status is 1 we keep the left array
     if(status == 1){
         free(info->A);
-        free(result.right);
-        free(result.middle);
+        free(result->right);
+        free(result->middle);
 
-        info->A = result.left;
-        info->size = result.leftSize;
+        info->A = result->left;
+        info->size = result->leftSize;
 
-         MPI_Bcast(
+        // Receive the status from the master process again
+        MPI_Bcast(
             &status,
             1,
             MPI_INT,
             master_rank,
             communicator
         );
-        //slaves stop here
+
+        // printf("\t\tProcess %d has %d points\n\t\t", info->world_rank, info->size);
+
+
+        // If the status is 4 master gathers the data and all the other processes stop
         if(status == 4){
+
+            // Gather the data from all the processes to the master process
+            MPI_Gatherv(
+                info->A,
+                info->size,
+                MPI_INT,
+                NULL,
+                NULL,
+                NULL,
+                MPI_INT,
+                master_rank,
+                communicator
+            );
+
+            // Free the memory
+            free(info->A);
+            free(result);
+
+            // and exit
             return;
         }
-        MPI_Gather(info->A, info->size, MPI_INT, gatheredData, info->size, MPI_INT, master_rank, communicator);
-
-
-
+        
     } else if(status == 2){
-        free(partitionResult);  
-        free(result.left);
-        free(result.right);
-        free(result.middle);
+        // If the status is the k-th smallest is the pivot
 
+        // free the memory
+        free(info->A);
+        free(result->left);
+        free(result->right);
+        free(result->middle);
+        free(result);
+
+        // and exit
         return;
 
     } else if(status == 3){
         free(info->A);
-        free(result.left);
-        free(result.middle);
+        free(result->left);
+        free(result->middle);
 
-        info->A = result.right;
-        info->size = result.rightSize;
-
-         MPI_Bcast(
+        info->A = result->right;
+        info->size = result->rightSize;
+        
+        MPI_Bcast(
             &status,
             1,
             MPI_INT,
             master_rank,
             communicator
         );
-        //slaves stop here
+        
+        // If the status is 4 master gathers the data and all the other processes stop
         if(status == 4){
+            
+            // Gather the data from all the processes to the master process
+            MPI_Gatherv(
+                info->A,
+                info->size,
+                MPI_INT,
+                NULL,
+                NULL,
+                NULL,
+                MPI_INT,
+                master_rank,
+                communicator
+            );
+
+            // Free the memory
+            free(info->A);
+            free(result);
+
+            // and exit
             return;
         }
     }
 
-     
 
+    // Balance the number of points that remain in the array of each process
+    balancePointsSlave(&info->A, &info->size, info->world_size, min_rank, max_rank, info->world_rank, communicator);
 
     slaveProcess(master_rank, min_rank, max_rank, info, communicator);
     
-    free(partitionResult);
+    free(result);
 }
--- src/mpi/slaveProcess.h
+++ src/mpi/slaveProcess.h
@@ -1,9 +1,8 @@
 #ifndef SLAVEPROCESS_H   /* Include guard */
 #define SLAVEPROCESS_H 
 
-#include <mpi.h>
 #include "structs.h"
-
+#include <mpi.h>
 
 void slaveProcess(int master_rank, int min_rank, int max_rank, Info *info, MPI_Comm communicator);
 
