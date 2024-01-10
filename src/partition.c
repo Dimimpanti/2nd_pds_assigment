cff406ddf43024a99e8cbfbe337db1cf703e8c1f
Δημητρα Μπαντη <dimimpanti@gmail.com>
2024-01-10 20:02:53 +0200

    Fixes merge conflicts

--- src/partition.c
+++ src/partition.c
@@ -9,12 +9,6 @@ PivotedArrays *partition(int* A, int size, int pivot) {
     result->left = (int*)malloc(size * sizeof(int));
     result->middle = (int*)malloc(size * sizeof(int));
     result->right = (int*)malloc(size * sizeof(int));
-PivotedArrays *partition(int* A, int size, int pivot) {
-    PivotedArrays *result = (PivotedArrays*)malloc(sizeof(PivotedArrays));
-
-    result->left = (int*)malloc(size * sizeof(int));
-    result->middle = (int*)malloc(size * sizeof(int));
-    result->right = (int*)malloc(size * sizeof(int));
 
     int leftIndex = 0, middleIndex = 0, rightIndex = 0;
 
@@ -22,23 +16,16 @@ PivotedArrays *partition(int* A, int size, int pivot) {
         //Elements less than pivot
         if(A[i] < pivot){
             result->left[leftIndex++] = A[i];
-            result->left[leftIndex++] = A[i];
         }
         //Elements equal to pivot
         else if(A[i] == pivot){
-            result->middle[middleIndex++] = A[i];
             result->middle[middleIndex++] = A[i];
         }
         //Elements greater than pivot
         else{
             result->right[rightIndex++] = A[i];
-            result->right[rightIndex++] = A[i];
         }
     }
-
-    result->leftSize = leftIndex;
-    result->middleSize = middleIndex;
-    result->rightSize = rightIndex;
 
     result->leftSize = leftIndex;
     result->middleSize = middleIndex;
@@ -61,25 +48,9 @@ int kselect(int* A, int size, int k) {
     // Pivot is the first element of the array
     int pivot = A[0];
 
-
-/**
- * @brief  Find the k-th smallest element in an array
- * 
- * @param A     The array
- * @param size  The size of the array
- * @param k     The k-th smallest element
- * @return int 
- */
-int kselect(int* A, int size, int k) {
-
-    // Pivot is the first element of the array
-    int pivot = A[0];
-
-
     if (k > 0 && k <= size) {
     
         // Partition the array using the pivot
-        PivotedArrays *result = partition(A, size, pivot);
         PivotedArrays *result = partition(A, size, pivot);
 
         // Recursively find k-th smallest in the appropriate partition
@@ -92,22 +63,7 @@ int kselect(int* A, int size, int k) {
             free(result);
 
             return res;
-        if (k <= result->leftSize) {
-            int res = kselect(result->left, result->leftSize, k);
-
-            free(result->left);
-            free(result->middle);
-            free(result->right);
-            free(result);
-
-            return res;
         
-        } else if (k <= result->leftSize + result->middleSize) {
-            free(result->left);
-            free(result->middle);
-            free(result->right);
-            free(result);
-
         } else if (k <= result->leftSize + result->middleSize) {
             free(result->left);
             free(result->middle);
@@ -115,17 +71,6 @@ int kselect(int* A, int size, int k) {
             free(result);
 
             return pivot;
-        
-        } else {
-            int res = kselect(result->right, result->rightSize, k - result->leftSize - result->middleSize);
-
-            free(result->left);
-            free(result->middle);
-            free(result->right);
-            free(result);
-
-            return res;
-        }
         
         } else {
             int res = kselect(result->right, result->rightSize, k - result->leftSize - result->middleSize);
